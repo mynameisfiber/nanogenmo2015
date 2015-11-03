1fbf941a3e98ce886d1df1299da4b88c03381cd6
Micha Gorelick <mynameisfiber@gmail.com>
2015-11-03 11:45:08 -0500

    Cleaner API for the book class

--- gutenburg.py
+++ gutenburg.py
@@ -4,15 +4,45 @@ done by first extracting out chapters and their respective contents and then
 scoring each section.  This'll allow us to get rid of some of the Gutenburg
 fluf.
 """
+
 from lxml import html
+import re
+import itertools as IT
+
+
+def clean_string(content):
+    content, _ = re.subn(r"[\r\n]+", " ", content.strip())
+    return content
 
 class Book(object):
+    """
+    Parses a Gutenburg project html file.  Currently only gets basic metadata
+    (title/author) and the content of the book, split up into chapters.
+    Currently, images are ignored.
+    """
     def __init__(self, filename):
         self.filename = filename
         self.dom = html.fromstring(open(filename).read())
-        self._parse()
+        self.chapters = self._parse_chapters()
+        self.meta = self._parse_meta()
 
-    def _parse(self):
+    def _parse_meta(self):
+        title_node = self.dom.xpath("/html/head/title")[0]
+        title = title_node.text_content().strip()
+        # Parse titles of the form "The Project Gutenberg eBook of Brigands of
+        # the Moon, by Ray Cummings".  This is done by first eliminating words
+        # starting with upper case, then words starting with lowercase, then
+        # assuming the title starts followed by the author.
+        match = re.search(
+            r"^([A-Z][a-zA-Z]+ )*([a-z][a-zA-Z]+ )*(?P<title>.+), by (?P<author>.+)$", 
+            title
+        )
+        if match is not None:
+            return match.groupdict()
+        else:
+            return {"title" : None, "author" : None}
+
+    def _parse_chapters(self):
         """
         This function should get all the metadata we need from a given dom
         structure.  Right now all we do is get rid of the license and then find
@@ -21,20 +51,24 @@ class Book(object):
         # remove the license
         for license in self.dom.xpath(".//pre"):
             license.getparent().remove(license)
+        # remove page labels
+        for page in self.dom.xpath(".//span[@class='pagenum']"):
+            page.getparent().remove(page)
         # find headers and parse their contents
         chapters = []
         headers = self.dom.xpath(".//*[self::h1 or self::h2 or self::h3 and p]")
         for header in headers:
             content = self._parse_header(header)
             score = self._score_content(content)
+            images = self._parse_images(content)
             if score > 0:
                 chapters.append({
                     "header" : header, 
                     "content" : content, 
                     "score" : score,
-                    "content" : [node.text_content().strip() for node in content],
+                    "content" : [clean_string(node.text_content()) for node in content],
                 })
-        self.chapters = chapters
+        return chapters
 
     def _parse_header(self, node):
         """
@@ -62,3 +96,10 @@ class Book(object):
         for node in content:
             score += len(node.text_content().strip()) / (1.0 + sum(1 for _ in node.iterdescendants()))
         return score / float(len(content))
+
+    def __len__(self):
+        return len(self.chapters)
+
+    def __repr__(self):
+        return '<gutenburg.book, "{title}" by {author}>'.format(**self.meta)
+
